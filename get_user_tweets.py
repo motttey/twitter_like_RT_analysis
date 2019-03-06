72124ba84daa57e42fd03c999a7d7702156db608
motttey <motitago@gmail.com>
2019-03-06 18:11:01 +0900

    get specific user's media tweets

--- get_user_tweets.py
+++ get_user_tweets.py
@@ -10,20 +10,27 @@ twitter = OAuth1Session(CK, CS, AT, ATS)
 
 # url = "https://api.twitter.com/1.1/users/show.json"
 # params = {"id": 23048655}
-url = "https://api.twitter.com/1.1/search/tweets.json"
+url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
 params = {
-    "q": "from:@motttey filter:images exclude:retweets",
-    # "result_type": "popular",
-    "count": 100,
+    "screen_name": "mt_tg",
+    "count": 300,
+    # "count": 100,
 }
 req = twitter.get(url, params=params)
 
 if req.status_code == 200:
     statuses = json.loads(req.text)
-    for status in statuses["statuses"]:
-        print(status["text"].encode('utf-8'))
-        print(status["retweet_count"])
-        print(status["favorite_count"])
-        print("---")
+    print(len(statuses))
+    print("---")
+
+    for status in statuses:
+        if status["favorite_count"] > 1 and "retweeted_status" not in status.keys():
+            if "media" in status["entities"].keys():
+                print(status["text"].encode('utf-8'))
+                print(status["retweet_count"])
+                print(status["favorite_count"])
+                print(status["retweet_count"] / status["favorite_count"])
+
+                print("---")
 else:
     print(req.status_code)
