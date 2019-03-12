3a8c4b8e6fc160c10830bbaa03e7d5aaee014354
motttey <motitago@gmail.com>
2019-03-12 23:26:57 +0900

    get tweet older than 200

--- get_user_tweets.py
+++ get_user_tweets.py
@@ -10,27 +10,40 @@ twitter = OAuth1Session(CK, CS, AT, ATS)
 
 # url = "https://api.twitter.com/1.1/users/show.json"
 # params = {"id": 23048655}
-url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-params = {
-    "screen_name": "mt_tg",
-    "count": 300,
-    # "count": 100,
-}
-req = twitter.get(url, params=params)
+page_max = 16
+user_timeline_url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+fav_RT_ratio_array = []
 
-if req.status_code == 200:
-    statuses = json.loads(req.text)
-    print(len(statuses))
-    print("---")
+for page_index in range(1,page_max):
+    params = {
+        "screen_name": "mt_tg",
+        "count": 200,
+        # "count": 100,
+        "page": page_index,
+    }
+    req = twitter.get(user_timeline_url, params=params)
 
-    for status in statuses:
-        if status["favorite_count"] > 1 and "retweeted_status" not in status.keys():
-            if "media" in status["entities"].keys():
-                print(status["text"].encode('utf-8'))
-                print(status["retweet_count"])
-                print(status["favorite_count"])
-                print(status["retweet_count"] / status["favorite_count"])
+    if req.status_code == 200:
+        statuses = json.loads(req.text)
+        print(len(statuses))
+        print("---")
 
-                print("---")
-else:
-    print(req.status_code)
+        for status in statuses:
+            if status["favorite_count"] > 1 and "retweeted_status" not in status.keys():
+                if "media" in status["entities"].keys():
+                    print(status["text"].encode('utf-8'))
+                    print(status["retweet_count"])
+                    print(status["favorite_count"])
+                    print(status["retweet_count"] / status["favorite_count"])
+                    print("---")
+
+                    fav_RT_ratio = {}
+                    fav_RT_ratio["retweet_count"] = status["retweet_count"]
+                    fav_RT_ratio["favorite_count"] = status["favorite_count"]
+                    fav_RT_ratio["ratio"] = status["retweet_count"] / status["favorite_count"]
+
+                    fav_RT_ratio_array.append(fav_RT_ratio)
+    else:
+        print(req.status_code)
+
+print(fav_RT_ratio_array)
