d3e3002b71d5d913d512277615baba01097823df
motttey <motitago@gmail.com>
2019-03-13 16:16:52 +0900

    get follower list of specific user

--- get_followers.py
+++ get_followers.py
@@ -9,10 +9,40 @@ ATS = config.ACCESS_TOKEN_SECRET
 twitter = OAuth1Session(CK, CS, AT, ATS)
 
 url = "https://api.twitter.com/1.1/followers/list.json"
-params = {}
+
+params = {
+    "screen_name": "mt_tg",
+}
 
 req = twitter.get(url, params=params)
 
+if req.status_code == 200:
+    user_object = json.loads(req.text.encode('utf-8'))
+    print(user_object["next_cursor"])
+    cursor = user_object["next_cursor"]
+
+    for user in user_object["users"]:
+        print(user["screen_name"])
+
+    while "next_cursor" in user_object:
+        url = "https://api.twitter.com/1.1/followers/list.json"
+        params = {
+            "screen_name": "mt_tg",
+            "cursor": cursor,
+        }
+        req = twitter.get(url, params=params)
+        user_object = json.loads(req.text.encode('utf-8'))
+
+        if req.status_code == 200:
+            for user in user_object["users"]:
+                print(user["screen_name"])
+        else:
+            print(req.status_code)
+
+else:
+    print(req.status_code)
+
+'''
 if req.status_code == 200:
     followers = json.loads(req.text)
     for follower in followers["users"]:
@@ -22,3 +52,4 @@ if req.status_code == 200:
         print(result_text.encode('utf-8'))
 else:
     print(req.status_code)
+'''
